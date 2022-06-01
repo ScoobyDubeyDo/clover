53522b5a0d4797fce5dfae2fd3ef09b5e11ab55f
aman <aman.dubey1998@gmail.com>
2022-06-02 02:14:28 +0530

    style: added like icon

--- src/hooks/useIcons.js
+++ src/hooks/useIcons.js
@@ -7,7 +7,6 @@ import { HiUserCircle } from "react-icons/hi";
 import { IoIosClose } from "react-icons/io";
 import {
 	IoHeartOutline,
-	IoHeartSharp,
 	IoSettingsOutline,
 	IoSunnySharp,
 } from "react-icons/io5";
@@ -158,6 +157,22 @@ const BrandLogo = () => {
 					</g>
 				</g>
 			</g>
+		</svg>
+	);
+};
+
+const IoHeartSharp = ({ size, ...props }) => {
+	return (
+		<svg width={size} height={size} viewBox="0 0 512 512" {...props}>
+			<defs>
+				<linearGradient id="lgrad" x1="21%" y1="100%" x2="79%" y2="0%">
+					<stop offset="0%" stopColor="#4dabf7" />
+					<stop offset="100%" stopColor="#a9e34b" />
+				</linearGradient>
+			</defs>
+			<path
+				fill="url('#lgrad')"
+				d="m256 448l-9-6c-42.78-28.57-96.91-60.86-137-108.32c-42.25-50-62.52-101.35-62-157C48.63 114.54 98.46 64 159.08 64c48.11 0 80.1 28 96.92 48.21C272.82 92 304.81 64 352.92 64c60.62 0 110.45 50.54 111.08 112.65c.56 55.68-19.71 107-62 157c-40.09 47.49-94.22 79.78-137 108.35Z"></path>
 		</svg>
 	);
 };
