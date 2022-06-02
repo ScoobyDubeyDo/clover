9f80121c5e933c239220924516425bef70727eec
aman <aman.dubey1998@gmail.com>
2022-06-02 14:55:10 +0530

    misc: tried pwa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 import { store } from "./app/store";
 import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
@@ -19,9 +20,10 @@ ReactDOM.render(
 			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>,
-	document.getElementById("root"),
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
