3876cb422ac26dad1ae6c961ab3b646aa32dd8db
aman <aman.dubey1998@gmail.com>
2022-06-02 16:21:38 +0530

    ofline support part 2

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,8 +8,14 @@
 import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
 import { createHandlerBoundToURL, precacheAndRoute } from "workbox-precaching";
+// Any other custom service worker logic can go here.
+// ----------------------
+// Incrementing OFFLINE_VERSION will kick off the install event and force
+import { offlineFallback } from "workbox-recipes";
 import { registerRoute } from "workbox-routing";
+import { setDefaultHandler } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
+import { NetworkOnly } from "workbox-strategies";
 
 clientsClaim();
 
@@ -68,79 +74,14 @@ self.addEventListener("message", (event) => {
 	}
 });
 
-// Any other custom service worker logic can go here.
+// Asset hashes to see if content has changed.
+const assetHashes = self.__WB_MANIFEST;
+console.log(assetHashes);
 
-// ----------------------
+// Sets a default Network Only handler, but consider writing your own handlers, too!
+setDefaultHandler(new NetworkOnly());
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
-// Customize this with a different URL if needed.
-const OFFLINE_URL = "offline.html";
-
-self.addEventListener("install", (event) => {
-	event.waitUntil(
-		(async () => {
-			const cache = await caches.open(CACHE_NAME);
-			// Setting {cache: 'reload'} in the new request will ensure that the response
-			// isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-			await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
-		})()
-	);
-});
-
-self.addEventListener("activate", (event) => {
-	event.waitUntil(
-		(async () => {
-			// Enable navigation preload if it's supported.
-			// See https://developers.google.com/web/updates/2017/02/navigation-preload
-			if ("navigationPreload" in self.registration) {
-				await self.registration.navigationPreload.enable();
-			}
-		})()
-	);
-
-	// Tell the active service worker to take control of the page immediately.
-	self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-	// We only want to call event.respondWith() if this is a navigation request
-	// for an HTML page.
-	if (event.request.mode === "navigate") {
-		event.respondWith(
-			(async () => {
-				try {
-					// First, try to use the navigation preload response if it's supported.
-					const preloadResponse = await event.preloadResponse;
-					if (preloadResponse) {
-						return preloadResponse;
-					}
-
-					const networkResponse = await fetch(event.request);
-					return networkResponse;
-				} catch (error) {
-					// catch is only triggered if an exception is thrown, which is likely
-					// due to a network error.
-					// If fetch() returns a valid HTTP response with a response code in
-					// the 4xx or 5xx range, the catch() will NOT be called.
-					console.log(
-						"Fetch failed; returning offline page instead.",
-						error
-					);
-
-					const cache = await caches.open(CACHE_NAME);
-					const cachedResponse = await cache.match(OFFLINE_URL);
-					return cachedResponse;
-				}
-			})()
-		);
-	}
-
-	// If our if() condition is false, then this fetch handler won't intercept the
-	// request. If there are any other fetch handlers registered, they will get a
-	// chance to call event.respondWith(). If no fetch handlers call
-	// event.respondWith(), the request will be handled by the browser as if there
-	// were no service worker involvement.
+// HTML to serve when the site is offline
+offlineFallback({
+	pageFallback: "/offline.html",
 });
